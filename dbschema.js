fb2faabbed9f0561d362c79c4c132e5adf5a4e98
mcombs289 <maddy.combs4@gmail.com>
2022-09-13 19:31:54 -0400

    Express & formatting response

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  flutter: [
+    {
+      userHandle: "user",
+      body: "this is body ",
+      likeCount: 5,
+      commentCount: 3,
+      createdAt: "2019-03-15T11:46:01:018Z",
+    },
+  ],
+};
--- functions/index.js
+++ functions/index.js
@@ -3,40 +3,31 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello Maddy!");
-});
+const express = require("express");
 
-//getting documents
-exports.getFlutters = functions.https.onRequest((request, response) => {
+const app = express();
+
+app.get("/flutter", (req, res) => {
   admin
     .firestore()
     .collection("flutter")
+    .orderBy("createdAt", "desc") //order by created dat
     .get()
     .then((data) => {
       let flutters = [];
       data.forEach((doc) => {
         flutters.push(doc.data());
       });
-      return response.json(flutters);
+      return res.json(flutters);
     })
     .catch((err) => console.error(err));
 });
 
-//creating documents
-exports.createFlutter = functions.https.onRequest((request, response) => {
-  if (request.method !== "POST") {
-    return response.status(400).json({ error: "Method not allowed" });
-  }
-
+app.post("/flutter", (req, res) => {
   const newFlutter = {
-    body: request.body.body,
-    userHandle: request.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
   };
 
   admin
@@ -44,10 +35,13 @@ exports.createFlutter = functions.https.onRequest((request, response) => {
     .collection("flutter")
     .add(newFlutter)
     .then((doc) => {
-      response.json({ message: `document ${doc.id} created successfully` });
+      res.json({ message: `document ${doc.id} created successfully` });
     })
     .catch((err) => {
-      response.status(500).json({ error: "something went wrong" }); // server error
+      res.status(500).json({ error: "something went wrong" }); // server error
       console.error(err);
     });
 });
+
+//
+exports.api = functions.https.onRequest(app);
